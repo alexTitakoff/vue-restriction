dd33d6fb0e3ae1d1f6f0db9511f38199e3b5596b
alex <alex.titakoff@yandex.ru>
2020-10-28 14:52:50 +0300

    vue app replace work

---
+++ src/views/Home.vue
@@ -0,0 +1,496 @@
+<template>
+  <div>
+
+
+    <div id="app">
+      <nav class="navbar navbar-light bg-light justify-content-between">
+        <div class="justify-content-start">
+          <a class="navbar-brand" href="#">
+            <img src="https://cdn.pixabay.com/photo/2016/09/20/03/12/yang-1681698_960_720.png" width="30"
+                 height="30"
+                 alt="" loading="lazy">
+          </a>
+          <div style="display:inline">Дзен Ограничения / {{user.nick}}</div>
+        </div>
+
+        <a href="" style="color: inherit;" @click.prevent='logout()'>Выйти</a>
+      </nav>
+      <div class="container">
+
+        <div v-if='user.login'>
+          <!-- Just an image -->
+
+          <!-- тоаст уведомление -->
+          <div class="toast" autohide="true" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-body">
+              {{toast}}
+            </div>
+          </div>
+
+
+          <!-- модалка добавления количества -->
+          <div class="modal" id="addingModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Добавление</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Точно добавить {{changeDataItem.ruName}}?</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                  <button type="button" class="btn btn-primary" @click="addItemCount">Добавить
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- модалка  удаления количества -->
+          <div class="modal" id="removingModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Удаление</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <p>Точно удалить {{changeDataItem.ruName}}?</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                  <button type="button" class="btn btn-primary" @click="removeItemCount">Удалить
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <!-- модалка  добавления ограничения-->
+          <div class="modal" id="addRestrictModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Добавить Ограничение</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body">
+                  <div class="form-group">
+                    <label for="name">Название ограничения</label>
+                    <input v-model="newRestr.ruName" class="form-control">
+                    <label for="name">Максимальное число</label>
+                    <input type="number" v-model="newRestr.maxCount" class="form-control">
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Отмена</button>
+                  <button type="button" class="btn btn-primary" @click="addRestrict">Добавить</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-6">
+              <h3>Ограничения {{date}}</h3>
+              <button style="margin-left: 0;
+                        margin-bottom: 20px;
+                        margin-top: 5px;" type="button" class=" add-count btn btn-success" data-toggle="modal"
+                      data-target="#addRestrictModal">
+                Добавить Ограничение
+              </button>
+              <ul class="list-group ">
+                <li v-for="restriction in restrictions" class="list-group-item">
+                  <div class="row justify-content-between ">
+                    <div class=" col-6">
+                      {{restriction.ruName}}
+                      <span class="max-count">макс. кол-во: {{restriction.maxCount}}</span>
+                      <span class="limit" v-if="restriction.count > restriction.maxCount">превышено на
+                      {{restrictionDiff(restriction.count,restriction.maxCount)}} </span>
+                    </div>
+                    <div class="col-6 right-col">
+                      <p>Уже съедено: {{restriction.count}} </p>
+
+                      <button type="button" class=" add-count btn btn-success" data-toggle="modal"
+                              data-target="#addingModal" @click="addModalCount(restriction)">
+                        <i class="fa fa-plus" aria-hidden="true"></i>
+                      </button>
+                      <button type="button" class=" add-count btn btn-danger" data-toggle="modal"
+                              data-target="#removingModal" @click="removeModalCount(restriction)"
+                              v-if="restriction.count > 0">
+                        <i class="fa fa-minus" aria-hidden="true"></i>
+                      </button>
+                    </div>
+                  </div>
+
+                </li>
+
+              </ul>
+            </div>
+            <div v-if="fbData !== null" class="col-md-6">
+              <h3>Журнал</h3>
+              <p style="display: block; font-size: 12px; margin-bottom: 0px;" v-for="log in niceLogs">
+                {{log}}
+              </p>
+            </div>
+
+          </div>
+        </div>
+
+
+        <!-- Login -->
+
+        <form v-else @submit.prevent="login()">
+          <h3>Вход</h3>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Никнейм</label>
+            <input v-model="user.nick" class="form-control" id="exampleInputEmail1"
+                   aria-describedby="emailHelp">
+
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Код</label>
+            <input type="password" v-model="user.code" class="form-control" id="exampleInputPassword1">
+          </div>
+          <button type="submit" class="btn btn-primary">Войти</button>
+        </form>
+
+        <!--
+              <pre>
+                        {{$data}}
+                    </pre> -->
+      </div>
+
+
+    </div>
+
+
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+
+  export default {
+    name: 'Home',
+    components: {},
+    data:() => ({
+      user: {
+        nick: null,
+        code: null,
+        login: false,
+      },
+      restrictions: [],
+      changeDataItem: {
+        name: null,
+        ruName: null,
+      },
+      newRestr: {
+        name: null,
+        ruName: null,
+        maxCount: null,
+        count: 0
+      },
+      toast: null,
+      fbData: null,
+      ref: null,
+      dateMonthYear: null,
+      date: null,
+      dict: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    }),
+    mounted() {
+      console.log("VUE APP WORK")
+      const vm = this
+      //Проверка логина
+      this.checkLogin()
+
+      // Устанавливаем фунциональную дату
+      this.setDateSettings()
+      // Your web app's Firebase configuration
+      var firebaseConfig = {
+        apiKey: "AIzaSyCiHrYTwisX9wniYo8cN7nlc6bWLu0Vc8s",
+        authDomain: "vue-dzen-restriction.firebaseapp.com",
+        databaseURL: "https://vue-dzen-restriction.firebaseio.com",
+        projectId: "vue-dzen-restriction",
+        storageBucket: "vue-dzen-restriction.appspot.com",
+        messagingSenderId: "546683537555",
+        appId: "1:546683537555:web:f17d266c5f7bcc22846566",
+        measurementId: "G-GKRY0MQ9EV"
+      };
+      // Initialize Firebase
+      firebase.initializeApp(firebaseConfig)
+      firebase.analytics()
+      this.database = firebase.database()
+      // установка рефа
+      this.ref = firebase.database().ref();
+
+      // получение данных из базы
+      let fbData
+      this.ref.on("value", function (snapshot) {
+
+        fbData = snapshot.val()
+        vm.fbData = fbData
+
+        // установка ограничений
+        // let fbDataObj = fbData['data_' + vm.dateMonthYear].restrictions
+        // for(key in fbDataObj){
+        //   vm.restrictions.push(fbDataObj[key])
+        // }
+        console.log('data_' + vm.dateMonthYear)
+        vm.restrictions = fbData['data_' + vm.dateMonthYear].restrictions
+
+
+        // console.log(vm.fbData.logs)
+
+      }, function (error) {
+        console.log("Error: " + error.code);
+      })
+
+      //пример, для установки структуры
+      // this.database.ref('data_' + 'test').set({
+      //     'date': new Date(),
+      //     "restrictions": [
+      //         {
+      //             "count": 1,
+      //             "maxCount": 4,
+      //             "name": "shaurma",
+      //             "ruName": "Шаурма"
+      //         },
+      //         {
+      //             "count": 3,
+      //             "maxCount": 4,
+      //             "name": "chocolate",
+      //             "ruName": "Шоколадки"
+      //         }
+      //     ]
+
+      // });
+
+      // //users
+      // this.database.ref('users').set({
+      //     nika: {
+      //         code: 2
+      //     },
+      //     alex: {
+      //         code: 1
+      //     }
+      // })
+
+    },
+    computed: {
+      niceLogs() {
+        return JSON.parse(this.fbData.logs).slice().reverse();
+      },
+    },
+    methods: {
+      checkLogin: function () {
+        if (localStorage.login) {
+          console.log(localStorage.login)
+          this.user = JSON.parse(localStorage.login)
+        }
+      },
+      login: function () {
+        console.log(this.user)
+        if (this.fbData.users[this.user.nick] && this.user.code == this.fbData.users[this.user.nick].code) {
+          this.user.login = true
+          localStorage.setItem('login', JSON.stringify(this.user));
+        }
+        this.setLog('login')
+      },
+      logout: function () {
+        this.user = {
+          nick: null,
+          code: null,
+          login: false,
+        }
+        delete localStorage.login
+      },
+      setDateSettings: function () {
+        let d = new Date()
+        this.dateMonthYear = '' + d.getMonth() + d.getFullYear()
+        this.date = '/ ' + this.dict[d.getMonth()] + ' ' + d.getFullYear()
+
+        // // test
+        // this.dateMonthYear = 'test'
+        // this.date = 'test'
+
+      },
+      addRestrict: function () {
+        if (this.validateNewRestr() == false) {
+          this.toast = 'Поля должные быть заполнены'
+          $('.toast').toast('show')
+          return
+        }
+
+        let vm = this
+        this.newRestr.name = this.toTranslit(this.newRestr.ruName)
+        let newPostKey = this.database.ref('data_' + vm.dateMonthYear).child('restrictions').push().key
+        let updates = {};
+        updates['/restrictions/' + newPostKey] = this.newRestr
+        this.database.ref('data_' + vm.dateMonthYear).update(updates)
+
+        $('#addRestrictModal').modal('hide')
+        vm.setLog('addRestrict')
+      },
+      //Добавление и удаление количества огранчений
+      addModalCount: function (restriction) {
+        this.changeDataItem.name = restriction.name
+        this.changeDataItem.ruName = restriction.ruName
+
+      },
+      removeModalCount: function (restriction) {
+        this.changeDataItem.name = restriction.name
+        this.changeDataItem.ruName = restriction.ruName
+
+      },
+      addItemCount: function () {
+        const vm = this
+        for (key in this.restrictions) {
+          if (this.restrictions[key].name == vm.changeDataItem.name) {
+            this.restrictions[key].count++
+            let updates = {};
+            updates['/restrictions/' + key] = this.restrictions[key];
+            this.database.ref('data_' + vm.dateMonthYear).update(updates)
+
+            $('#addingModal').modal('hide')
+            vm.setLog('addItemCount')
+          }
+        }
+
+        console.log()
+        return
+
+        this.restrictions.map(function (restriction) {
+          if (restriction.name == vm.changeDataItem.name) {
+            restriction.count++
+            $('#addingModal').modal('hide')
+            vm.updateData()
+            vm.setLog('addItemCount')
+          }
+          // return name.length;
+        });
+      },
+      removeItemCount: function () {
+        const vm = this
+        for (key in this.restrictions) {
+          if (this.restrictions[key].name == vm.changeDataItem.name) {
+            this.restrictions[key].count--
+            let updates = {};
+            updates['/restrictions/' + key] = this.restrictions[key];
+            this.database.ref('data_' + vm.dateMonthYear).update(updates)
+
+            $('#removingModal').modal('hide')
+            vm.setLog('addItemCount')
+          }
+        }
+      },
+      updateData: function () {                 // Deprecate or update this func
+        const vm = this
+        this.database.ref('data_' + vm.dateMonthYear).set({
+          restrictions: this.restrictions
+        });
+
+      },
+      // Служебные методы
+      validateNewRestr: function () {
+        if (this.newRestr.ruName == null || this.newRestr.maxCount == null) {
+          return false
+        }
+        return true
+      },
+      toTranslit: function (text) {
+        return text.replace(/([а-яё])|([\s_-])|([^a-z\d])/gi,
+            function (all, ch, space, words, i) {
+              if (space || words) {
+                return space ? '-' : '';
+              }
+              var code = ch.charCodeAt(0),
+                  index = code == 1025 || code == 1105 ? 0 :
+                      code > 1071 ? code - 1071 : code - 1039,
+                  t = ['yo', 'a', 'b', 'v', 'g', 'd', 'e', 'zh',
+                    'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p',
+                    'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh',
+                    'shch', '', 'y', '', 'e', 'yu', 'ya'
+                  ];
+              return t[index];
+            });
+      },
+      setLog: function (par, val, db) {
+        let logs = JSON.parse(this.fbData.logs)
+        if (par == 'login') {
+          logs.push(this.formatDate(new Date()) + ': юзер ' + this.user.nick + ' зашел ')
+          this.database.ref('logs').set(JSON.stringify(logs))
+        }
+
+        if (par == 'addItemCount') {
+          logs.push(this.formatDate(new Date()) + ': юзер ' + this.user.nick + ' добавил ' + this.changeDataItem.ruName + ' в базу ' + 'data_' + this.dateMonthYear)
+          this.database.ref('logs').set(JSON.stringify(logs))
+        }
+
+        if (par == 'removeItemCount') {
+          logs.push(this.formatDate(new Date()) + ': юзер ' + this.user.nick + ' удалил ' + this.changeDataItem.ruName + ' из базы ' + 'data_' + this.dateMonthYear)
+          this.database.ref('logs').set(JSON.stringify(logs))
+        }
+
+        if (par == 'addRestrict') {
+          logs.push(this.formatDate(new Date()) + ': юзер ' + this.user.nick + ' добавил ограничение ' + this.newRestr.ruName + ' в базу ' + 'data_' + this.dateMonthYear)
+          this.database.ref('logs').set(JSON.stringify(logs))
+        }
+
+      },
+      restrictionDiff(count, max) {
+        return count - max
+      },
+      formatDate: function (d) {
+        const ye = new Intl.DateTimeFormat('ru', {year: 'numeric'}).format(d)
+        const mo = new Intl.DateTimeFormat('ru', {month: '2-digit'}).format(d)
+        const da = new Intl.DateTimeFormat('ru', {day: '2-digit'}).format(d)
+        const h = new Intl.DateTimeFormat('ru', {hour: '2-digit'}).format(d)
+        const m = new Intl.DateTimeFormat('ru', {minute: '2-digit'}).format(d)
+        const s = new Intl.DateTimeFormat('ru', {second: '2-digit'}).format(d)
+        return da + "/" + mo + "/" + ye + ' ' + h + ':' + m + ':' + s
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .max-count {
+    display: block;
+    font-size: 11px;
+  }
+
+  .limit {
+    display: block;
+    font-size: 11px;
+    color: lightcoral;
+  }
+
+  .right-col {
+    justify-content: flex-end;
+    display: flex;
+    align-items: center;
+  }
+
+  .add-count {
+    margin-left: 10px;
+  }
+
+  .toast {
+    position: absolute;
+    z-index: 99999999999999;
+    right: 10px;
+  }
+
+  p {
+    display: contents;
+  }
+</style>
